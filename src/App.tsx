6de730f9f628fc03b42c83c2925361c2e2712b25
FatahChan <ahmadfathallah89@gmail.com>
2024-12-13 11:48:36 +0200

    test release

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite and React logos to learn more, Test2
       </p>
        <button onClick={() => {throw new Error("This is your first error!");}}>Break the world</button>
     </>
